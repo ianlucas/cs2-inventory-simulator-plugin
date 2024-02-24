193cb9300858c584424c6f667e50a9848b028947
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-02-23 23:00:57 -0300

    chore: mit license

--- InventorySimulator.cs
+++ InventorySimulator.cs
@@ -1,6 +1,6 @@
 ﻿/*---------------------------------------------------------------------------------------------
  *  Copyright (c) Ian Lucas. All rights reserved.
- *  Licensed under the GPL-3.0 License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 using CounterStrikeSharp.API;
